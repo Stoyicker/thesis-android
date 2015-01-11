af823406f4eb12838bf30e3cb819f90354edded8
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2015-01-11 14:34:02 +0100

    Added the view that will represent the attachments

---
+++ app/src/main/java/org/thesis/android/ui/component/attachment/AttachmentView.java
@@ -0,0 +1,50 @@
+package org.thesis.android.ui.component.attachment;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import org.thesis.android.R;
+
+@SuppressLint("ViewConstructor") //They wouldn't be used anyway
+public class AttachmentView extends CardView implements View.OnClickListener {
+
+    private final String mAtachmentName;
+    private final IOnAttachmentRemovedListener mCallback;
+
+    public AttachmentView(Context context, String attachmentName, IOnAttachmentRemovedListener
+            _callback) {
+        super(context);
+        View v = ((LayoutInflater) context.getSystemService(Context
+                .LAYOUT_INFLATER_SERVICE)).inflate(R.layout.custom_view_attachment_card, this,
+                Boolean.TRUE);
+
+
+        final TextView textView = (TextView) v.findViewById(R.id.attachment_name);
+        textView.setText(attachmentName);
+        mCallback = _callback;
+        mAtachmentName = attachmentName;
+
+
+        setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        v.setVisibility(View.GONE);
+        if (mCallback != null)
+            mCallback.onAttachmentRemoved(this);
+    }
+
+    public String getName() {
+        return mAtachmentName;
+    }
+
+    public interface IOnAttachmentRemovedListener {
+
+        public void onAttachmentRemoved(AttachmentView removed);
+    }
+}
