2bccfc4dbe09d75caf1272e43e76d70d409cb817
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2015-01-25 14:10:04 +0100

    Bugfixes

--- app/src/main/java/org/thesis/android/ui/activity/DataProvisionActivity.java
+++ app/src/main/java/org/thesis/android/ui/activity/DataProvisionActivity.java
@@ -5,13 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.text.Editable;
 import android.text.TextUtils;
-import android.text.TextWatcher;
-import android.view.View;
-import android.view.animation.TranslateAnimation;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import org.thesis.android.CApplication;
 import org.thesis.android.R;
@@ -31,67 +30,41 @@ public class DataProvisionActivity extends ActionBarActivity {
 
         final Context context = CApplication.getInstance().getContext();
         final EditText nameField = (EditText) findViewById(R.id.name_field);
-        final View buttonProceed = findViewById(R.id.button_proceed);
-
-        nameField.addTextChangedListener(new TextWatcher() {
+        nameField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int
-                    start, int count, int after) {
-                //Do nothing
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                //Do nothing
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (buttonProceed.isShown() && TextUtils.isEmpty(s.toString())) {
-                    TranslateAnimation animate = new TranslateAnimation(0, 0, 0,
-                            buttonProceed.getHeight());
-                    animate.setDuration(context.getResources().getInteger(R.integer
-                            .floating_label_layout_duration_millis));
-                    animate.setFillAfter(Boolean.TRUE);
-                    buttonProceed.startAnimation(animate);
-                    buttonProceed.setVisibility(View.INVISIBLE);
-                } else if (!buttonProceed.isShown() && !TextUtils.isEmpty(s.toString())) {
-                    TranslateAnimation animate = new TranslateAnimation(0, 0,
-                            buttonProceed.getHeight(), 0);
-                    animate.setDuration(context.getResources().getInteger(R.integer
-                            .floating_label_layout_duration_millis));
-                    animate.setFillAfter(Boolean.TRUE);
-                    buttonProceed.startAnimation(animate);
-                    buttonProceed.setVisibility(View.VISIBLE);
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE || (event != null && event
+                        .isShiftPressed() && (event
+                        .getAction() == KeyEvent
+                        .ACTION_DOWN &&
+                        event.getKeyCode() == KeyEvent.KEYCODE_ENTER))) {
+                    if (TextUtils.isEmpty(v.getText()))
+                        return Boolean.FALSE;
+                    final String name = nameField.getText().toString();
+                    PreferenceAssistant.saveSharedString(context,
+                            PreferenceAssistant.PREF_USER_NAME, name);
+                    SQLiteDAO.getInstance().addUserName(name);
+                    PreferenceAssistant.saveSharedBoolean(context,
+                            PreferenceAssistant.PREF_USER_HAS_SET_NAME, Boolean.TRUE);
+                    v.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            final InputMethodManager imm = (InputMethodManager) context
+                                    .getSystemService(
+                                            Context.INPUT_METHOD_SERVICE);
+                            imm.hideSoftInputFromWindow(nameField.getWindowToken(), 0);
+                            final Intent homeIntent = new Intent(context,
+                                    NavigationDrawerActivity.class);
+                            finish();
+                            startActivity(homeIntent);
+                            overridePendingTransition(R.anim.move_in_from_bottom,
+                                    R.anim.move_out_to_bottom);
+                        }
+                    });
+                    return Boolean.TRUE;
                 }
-            }
-        });
 
-        findViewById(R.id.button_proceed).setOnClickListener(new View
-                .OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final String name = nameField.getText().toString();
-                PreferenceAssistant.saveSharedString(context,
-                        PreferenceAssistant.PREF_USER_NAME, name);
-                SQLiteDAO.getInstance().addUserName(name);
-                PreferenceAssistant.saveSharedBoolean(context,
-                        PreferenceAssistant.PREF_USER_HAS_SET_NAME, Boolean.TRUE);
-                v.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        final InputMethodManager imm = (InputMethodManager) context
-                                .getSystemService(
-                                        Context.INPUT_METHOD_SERVICE);
-                        imm.hideSoftInputFromWindow(nameField.getWindowToken(), 0);
-                        final Intent homeIntent = new Intent(context,
-                                NavigationDrawerActivity.class);
-                        finish();
-                        startActivity(homeIntent);
-                        overridePendingTransition(R.anim.move_in_from_bottom,
-                                R.anim.move_out_to_bottom);
-                    }
-                });
+                return Boolean.FALSE;
             }
         });
     }
